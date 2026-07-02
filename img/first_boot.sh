9a6e5144a9de1406d3bbd4a8029f91ebc8897479
Allie Fitter <fitterj@gmail.com>
2026-07-01 23:39:20 -0500

    Update first boot

--- img/first_boot.sh
+++ img/first_boot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -sSL https://raw.githubusercontent.com/alliefitter/calvin_and_hobbes_viewer/refs/heads/main/scripts/install.sh | bash -
+curl -sSL https://raw.githubusercontent.com/alliefitter/shazam/refs/heads/main/bin/install.sh | bash -
